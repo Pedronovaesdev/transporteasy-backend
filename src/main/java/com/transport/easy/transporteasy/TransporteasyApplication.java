eb84d255de9b94479b8490a68d866769c42b3a5d
Pedronovaesdev <pedro.novaes@souunit.com.br>
2025-09-03 22:48:32 -0300

    innital commit: create files

---
+++ src/main/java/com/transport/easy/transporteasy/TransporteasyApplication.java
@@ -0,0 +1,13 @@
+package com.transport.easy.transporteasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransporteasyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransporteasyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/transport/easy/transporteasy/TransporteasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transport.easy.transporteasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransporteasyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
