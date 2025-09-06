7ef4a9b8aefe18aefec12da2fe3e39acbc85ac72
Roque <165859108+Fuelwild796@users.noreply.github.com>
2025-09-05 22:28:44 -0300

    update passenger: created entity passenger

---
+++ src/main/java/com/transport/easy/transporteasy/entities/Passageiro.java
@@ -0,0 +1,24 @@
+package com.transport.easy.transporteasy.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_passageiro")
+public class Passageiro implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+}
