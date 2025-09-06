2307183e1f7e9c5d762a1cf1e7ea98aae0020c2e
Roque <165859108+Fuelwild796@users.noreply.github.com>
2025-09-05 22:34:21 -0300

    update Passenger: created passenger repository

---
+++ src/main/java/com/transport/easy/transporteasy/repositories/PassageiroRepository.java
@@ -0,0 +1,7 @@
+package com.transport.easy.transporteasy.repositories;
+
+import com.transport.easy.transporteasy.entities.Passageiro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PassageiroRepository extends JpaRepository<Passageiro, Long> {
+}
