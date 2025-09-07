16a6579a9e5f26ea20357a20ee890e46e8d047b8
Roque <165859108+Fuelwild796@users.noreply.github.com>
2025-09-07 18:13:22 -0300

    Update Route: created Route entity,RouteDTO and RouteRepository

---
+++ src/main/java/com/transport/easy/transporteasy/entities/Rota.java
@@ -0,0 +1,23 @@
+package com.transport.easy.transporteasy.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_rota")
+public class Rota implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String rua;
+    private String bairro;
+    private String cidade;
+    private int numero;
+}
---
+++ src/main/java/com/transport/easy/transporteasy/repositories/RotaRepository.java
@@ -0,0 +1,7 @@
+package com.transport.easy.transporteasy.repositories;
+
+import com.transport.easy.transporteasy.entities.Rota;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RotaRepository extends JpaRepository<Rota, Integer> {
+}
